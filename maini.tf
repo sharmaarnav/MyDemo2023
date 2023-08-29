e639ceb1318d677a81dfc5e322ed05c0f2d2087b
Arnav Sharma <35055863+sharmaarnav@users.noreply.github.com>
2023-08-30 09:26:15 +1000

    Update maini.tf

--- maini.tf
+++ maini.tf
@@ -8,7 +8,11 @@ terraform {
 }
 
 provider "azurerm" {
-  # Configuration options
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  features {}
 }
 
 
