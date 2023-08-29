3030275d1b1fa754a4a2bac125973d5f2976f828
Arnav Sharma <35055863+sharmaarnav@users.noreply.github.com>
2023-08-30 09:23:57 +1000

    Update maini.tf

--- maini.tf
+++ maini.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.71.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
+
+
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
